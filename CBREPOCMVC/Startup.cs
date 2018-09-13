4e56296e00187c89f95c8ec3bf3be2fe308144ab
SAMBIT <SAMBIT@DESKTOP-3HB41U5>
2018-09-12 22:21:36 -0400

    changed the startup

--- CBREPOCMVC/Startup.cs
+++ CBREPOCMVC/Startup.cs
@@ -43,7 +43,7 @@ namespace CBREPOCMVC
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Login}/{id?}");
+                    template: "{controller=Home}/{action=DashBoard}/{id?}");
             });
         }
     }
